6573e092919d284e21fe77d19338025abd53bd65
Song Liu <leopku@qq.com>
2011-06-23 18:22:25 +0800

    fixed some small issue.

--- ha_kickoff.py
+++ ha_kickoff.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 
-##############################################
-# HAProxy kickoff is a kit for enable/disable
-# servers from haproxy.cfg
-#
-# Author: leopku@qq.com
-#
-# History:
-#   2011-06-22:
-#       + first create this kit. add class 
-#         AppServer & HAProxy. and test code.
-#   2011-06-23:
-#       + add argument parser to using easily
-#         under console.
-#       + add --dry-run and --test mode.
-##############################################
+__author__ = 'leopku@qq.com'
+__version__ = '%prog 1.0'
+__doc__ = """HAProxy kickoff is a console kit for enable/disable
+server(s) from haproxy.
+Author: %s
+Version: %s
+
+History:
+    2011-06-22:
+        + first create this kit. add class 
+          AppServer & HAProxy. and test code.
+    2011-06-23:
+        + add argument parser to using easily
+          under console.
+          add --dry-run and --test mode.
+""" % (__author__, __version__)
 
 import os
 import os.path
@@ -22,9 +23,6 @@ import sys
 import re
 import optparse
 import shutil
-
-__author__ = 'leopku@qq.com'
-__version__ = '%prog 1.0'
 
 class AppServer:
     def __init__(self, ip, port, is_enable=False):
@@ -82,16 +80,16 @@ if __name__ == "__main__":
     #
     __HAProxyReloadCmd__ = '/etc/init.d/haproxy reload'
     __usage__ = 'python %prog [--ip=ip] [--port=port] <--enable|disable> <--dry-run> <--reload> <--test>'
-    __desc__ = ''
+    __desc__ = 'HAProxy kickoff is a console kit for enable/disable server(s) from haproxy written by leo(leopku#qq.com).Feel free for this smart kit.'
 
-    parser = optparse.OptionParser(usage=__usage__, version=__version__, description=__desc__)
-    parser.add_option('--ip', metavar='192.168.0.123', help='Ip address which to be enabled/disabled. comma seperated for multi ip.')
-    parser.add_option('--port', metavar='321', help='Port number of one ip which to be enabled/disabled. NOTE: this parameter on work with single ip.')
+    parser = optparse.OptionParser(usage=__usage__, version=__version__, description=__desc__,)
+    parser.add_option('--ip', metavar='192.168.0.123', help='Ip address which to be enabled/disabled. Comma seperated for multi ip.')
+    parser.add_option('--port', metavar='321', help='Port number of one ip which to be enabled/disabled. NOTE: this parameter only work with single ip.')
     parser.add_option('-e', '--enable', dest='switch_status', action='store_true', help='Enable a service listenning on ip(s)[:port]. This option is OFF by default.')
     parser.add_option('-d', '--disable', dest='switch_status', action='store_false', help='Disable a service listenning on ip(s)[:port]. This option is ON by default. Do not specify both --enable and --disable was same as --disable.')
     parser.add_option('--reload', action='store_true', help='Reload haproxy after services enabled/disabled.')
     parser.add_option('--dry-run', dest='dry', action='store_true', help='Only print new config and any changes should NOT save to config file.')
-    parser.add_option('--test', action='store_true', help='Make a copy of haproxy config file to the same folder of this script for testing. Any changes will save to the copy.')
+    parser.add_option('--test', action='store_true', help='Make a copy of haproxy config file to the same folder of this script for testing. Any read/write should affect in the copy version.')
     opts, args = parser.parse_args()
 
     if opts.ip is None:
